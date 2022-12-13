459aa3966bf2d8377353657c1e867c4e9385986d
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-12-13 14:06:37 +0530

    Sarthak | Changes the logic of RandomCorrelationIdGenerator, uses thread_rng() everytime

--- raft/src/net/connect/random_correlation_id_generator.rs
+++ raft/src/net/connect/random_correlation_id_generator.rs
@@ -1,25 +1,17 @@
-use std::cell::RefCell;
-use std::ops::DerefMut;
-use rand::prelude::ThreadRng;
 use rand::{Rng, thread_rng};
 use crate::net::connect::correlation_id::{CorrelationId, CorrelationIdGenerator};
 
-pub struct RandomCorrelationIdGenerator {
-    thread_local_generator: RefCell<ThreadRng>,
-}
+pub struct RandomCorrelationIdGenerator {}
 
 impl CorrelationIdGenerator for RandomCorrelationIdGenerator {
     fn generate(&self) -> CorrelationId {
-        return self.thread_local_generator.borrow_mut().deref_mut().gen();
+        return thread_rng().gen();
     }
 }
 
 impl RandomCorrelationIdGenerator {
     pub fn new() -> Self {
-        let thread_local_generator = thread_rng();
-        return RandomCorrelationIdGenerator {
-            thread_local_generator: RefCell::new(thread_local_generator)
-        };
+        return RandomCorrelationIdGenerator {};
     }
 }
 
