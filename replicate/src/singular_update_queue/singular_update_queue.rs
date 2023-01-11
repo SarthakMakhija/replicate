e8977fd3afcecb36ed22fc7eb61bd2fa0d2714dc
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-01-11 18:02:42 +0530

    Sarthak | Introduces add_async and add_spawn methods in SingularUpdateQueue and corrects the implementation of SingularUpdateQueue

--- replicate/src/singular_update_queue/singular_update_queue.rs
+++ replicate/src/singular_update_queue/singular_update_queue.rs
@@ -1,37 +1,85 @@
 use std::future::Future;
+use std::pin::Pin;
 
 use tokio::runtime::{Builder, Runtime};
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::mpsc::error::SendError;
 use tokio::task::JoinHandle;
 
+pub(crate) struct Task {
+    block: Pin<Box<dyn Future<Output=()> + Send>>,
+}
+
 pub(crate) struct SingularUpdateQueue {
-    thread_pool: Runtime,
+    sender: Sender<Task>,
+    single_thread_pool: Runtime,
+    task_submission_pool: Runtime,
 }
 
 impl SingularUpdateQueue {
     pub(crate) fn new() -> SingularUpdateQueue {
-        let thread_pool = Builder::new_multi_thread()
+        let single_thread_pool = Builder::new_multi_thread()
             .worker_threads(1)
             .enable_all()
             .build()
             .unwrap();
 
-        return SingularUpdateQueue { thread_pool };
+        let (sender, receiver) = mpsc::channel::<Task>(100);
+        Self::spin(&single_thread_pool, receiver);
+
+        return SingularUpdateQueue {
+            sender,
+            single_thread_pool,
+            task_submission_pool: Builder::new_multi_thread()
+                .worker_threads(10)
+                .enable_all()
+                .build()
+                .unwrap(),
+        };
+    }
+
+    pub(crate) async fn add_async<F>(&self, handler: F) -> Result<(), SendError<Task>>
+        where
+            F: Future<Output = ()> + Send + 'static {
+        let block = Box::pin(handler);
+        return self.sender.clone().send(Task { block }).await;
+    }
+
+    pub(crate) fn add_spawn<F>(&self, handler: F) -> JoinHandle<Result<(), SendError<Task>>>
+        where
+            F: Future<Output = ()> + Send + 'static {
+        let block = Box::pin(handler);
+        let sender = self.sender.clone();
+
+        return self.task_submission_pool.spawn(async move {
+            return sender.send(Task { block }).await;
+        });
     }
 
     pub(crate) fn submit<F>(&self, handler: F)
         where
             F: Future + Send + 'static,
-            F::Output: Send + 'static { self.thread_pool.spawn(handler); }
+            F::Output: Send + 'static { self.single_thread_pool.spawn(handler); }
 
     pub(crate) fn add<F>(&self, handler: F) -> JoinHandle<<F as Future>::Output>
         where
             F: Future + Send + 'static,
             F::Output: Send + 'static {
-        return self.thread_pool.spawn(handler);
+        return self.single_thread_pool.spawn(handler);
     }
 
     pub(crate) fn shutdown(self) {
-        let _ = self.thread_pool.shutdown_background();
+        let _ = self.single_thread_pool.shutdown_background();
+        let _ = self.task_submission_pool.shutdown_background();
+    }
+
+    fn spin(thread_pool: &Runtime, mut receiver: Receiver<Task>) {
+        thread_pool.spawn(async move {
+            while let Some(task) = receiver.recv().await {
+                task.block.await;
+            }
+        });
     }
 }
 
@@ -51,10 +99,10 @@ mod tests {
         let singular_update_queue = SingularUpdateQueue::new();
 
         let (sender, mut receiver) = mpsc::channel(1);
-        singular_update_queue.submit(async move {
+        let _ = singular_update_queue.add_async(async move {
             storage.write().unwrap().insert("WAL".to_string(), "write-ahead log".to_string());
             sender.send(()).await.unwrap();
-        });
+        }).await;
 
         let _ = receiver.recv().await.unwrap();
         let read_storage = readable_storage.read().unwrap();
@@ -74,15 +122,15 @@ mod tests {
         let (sender_one, mut receiver_one) = mpsc::channel(1);
         let (sender_other, mut receiver_other) = mpsc::channel(1);
 
-        singular_update_queue.submit(async move {
+        let _ = singular_update_queue.add_async(async move {
             writable_storage.write().unwrap().insert("WAL".to_string(), "write-ahead log".to_string());
             sender_one.clone().send(()).await.unwrap();
-        });
+        }).await;
 
-        singular_update_queue.submit(async move {
+        let _ = singular_update_queue.add_async(async move {
             storage.write().unwrap().insert("RAFT".to_string(), "consensus".to_string());
             sender_other.clone().send(()).await.unwrap();
-        });
+        }).await;
 
         let _ = receiver_one.recv().await.unwrap();
         let _ = receiver_other.recv().await.unwrap();
@@ -100,14 +148,47 @@ mod tests {
         let storage = Arc::new(RwLock::new(HashMap::new()));
         let singular_update_queue = SingularUpdateQueue::new();
 
-        let handle = singular_update_queue.add(async move {
+        let (sender_one, mut receiver_one) = mpsc::channel(1);
+        let _ = singular_update_queue.add_spawn(async move {
             storage.write().unwrap().insert("WAL".to_string(), "write-ahead log".to_string());
-            return ("WAL".to_string(), "write-ahead log".to_string());
+            let _ = sender_one.send(("WAL".to_string(), "write-ahead log".to_string())).await;
         });
 
-        let (key, value) = handle.await.unwrap();
+        let (key, value) = receiver_one.recv().await.unwrap();
         assert_eq!("WAL", key);
         assert_eq!("write-ahead log", value);
+
+        singular_update_queue.shutdown();
+    }
+
+    #[tokio::test]
+    async fn submit_multiple_task_and_confirm_their_execution_in_order() {
+        let storage = Arc::new(RwLock::new(Vec::new()));
+        let writable_storage_one = storage.clone();
+        let writable_storage_two = storage.clone();
+        let readable_storage = storage.clone();
+        let singular_update_queue = SingularUpdateQueue::new();
+
+        let (sender_one, mut receiver_one) = mpsc::channel(1);
+        let (sender_other, mut receiver_other) = mpsc::channel(1);
+
+        let _ = singular_update_queue.add_async(async move {
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+            writable_storage_one.write().unwrap().push("WAL".to_string());
+            sender_one.clone().send(()).await.unwrap();
+        }).await;
+
+        let _ = singular_update_queue.add_async(async move {
+            tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+            writable_storage_two.write().unwrap().push("consensus".to_string());
+            sender_other.clone().send(()).await.unwrap();
+        }).await;
+
+        let _ = receiver_one.recv().await.unwrap();
+        let _ = receiver_other.recv().await.unwrap();
+
+        let read_storage = readable_storage.read().unwrap();
+        assert_eq!(vec!["WAL".to_string(), "consensus".to_string()], *read_storage);
 
         singular_update_queue.shutdown();
     }
