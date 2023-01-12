1358e332aa35b8493f17fde912799051ccd7e225
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-01-12 16:37:25 +0530

    Sarthak | Changes the timeout in switch_to_follower_on_heartbeat_response test

--- raft/src/state.rs
+++ raft/src/state.rs
@@ -617,7 +617,7 @@ mod tests {
             let cloned = inner_state.clone();
             let _ = inner_state.get_heartbeat_sender().await;
 
-            thread::sleep(Duration::from_millis(5));
+            thread::sleep(Duration::from_millis(8));
 
             assert_eq!(ReplicaRole::Follower, cloned.get_role());
             assert_eq!(5, cloned.get_term());
