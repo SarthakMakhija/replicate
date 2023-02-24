7f5d623655a66ecc67307633a0e7690412c4f4a2
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-02-24 18:21:43 +0530

    Sarthak | Changes the benchmark for singular update queue, it now adds more than 1M tasks to singular update queue

--- replicate/benches/benchmarks/singular_update_queue.rs
+++ replicate/benches/benchmarks/singular_update_queue.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, RwLock};
 
-use criterion::{Criterion, criterion_group};
+use criterion::{BatchSize, Criterion, criterion_group};
 use tokio::runtime::Builder;
 
 use replicate::singular_update_queue::singular_update_queue::SingularUpdateQueue;
@@ -12,6 +12,8 @@ struct State {
 struct Value {
     count: u64,
 }
+
+const SIZE: usize = 1024 * 1024;
 
 fn add(criterion: &mut Criterion) {
     let singular_update_queue = SingularUpdateQueue::new();
@@ -26,19 +28,25 @@ fn add(criterion: &mut Criterion) {
 
         bencher
             .to_async(runtime)
-            .iter(|| {
-                let inner_state = state.clone();
-                async {
-                    let result = singular_update_queue.add(async move {
-                        let mut value = inner_state.value.write().unwrap();
-                        value.count = value.count + 1;
-                    }).await;
+            .iter_batched(
+                || (0..SIZE).collect::<Vec<_>>(),
+                |ids| {
+                    async {
+                        for _id in ids {
+                            let inner_state = state.clone();
 
-                    if let Err(err) = result {
-                        panic!("error while submitting a task to singular_update_queue: {}", err.to_string());
+                            let result = singular_update_queue.add(async move {
+                                let mut value = inner_state.value.write().unwrap();
+                                value.count = value.count + 1;
+                            }).await;
+
+                            if let Err(err) = result {
+                                panic!("error while submitting a task to singular_update_queue: {}", err.to_string());
+                            }
+                        }
                     }
-                }
-            });
+                }, BatchSize::SmallInput,
+            );
     });
 }
 
